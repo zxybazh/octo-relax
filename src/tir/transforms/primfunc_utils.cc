c4ab2834119f0827d497c9775b695aba4b0d2f8f
Xiyou Zhou <xiyou@octoml.ai>
2023-02-16 22:24:28 -0800

    Finish cherry-pick.

--- src/tir/transforms/primfunc_utils.cc
+++ src/tir/transforms/primfunc_utils.cc
@@ -30,10 +30,15 @@ namespace tir {
 namespace transform {
 transform::Pass BindTarget(Target target) {
   auto fpass = [target](tir::PrimFunc f, IRModule m, transform::PassContext ctx) {
+    if (f->GetAttr<Integer>(tvm::tir::attr::kIsHostFunc) == 1) {
+      ICHECK(target->host.defined());
+      return WithAttr(std::move(f), tvm::attr::kTarget, target->host.value());
+    }
     return WithAttr(std::move(f), tvm::attr::kTarget, target);
   };
   return tir::transform::CreatePrimFuncPass(fpass, 0, "tir.BindTarget", {});
 }
+
 
 transform::Pass AnnotateEntryFunc() {
   auto fpass = [](tir::PrimFunc f, IRModule m, transform::PassContext ctx) {
